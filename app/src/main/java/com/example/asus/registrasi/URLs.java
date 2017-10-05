0526584618f2f096c1f87e4ad2fc08bb3c348919
Arvianti Yulia <arviantiyulia16@gmail.com>
2017-10-05 22:09:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asus/registrasi/URLs.java
@@ -0,0 +1,12 @@
+package com.example.asus.registrasi;
+
+/**
+ * Created by ASUS on 9/26/2017.
+ */
+
+public class URLs {
+    private static final String ROOT_URL = "http://192.168.0.103/android_coba/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
